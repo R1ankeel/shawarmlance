95deeb27134364f3153c81c7e2f2aaf9eabca7d4
R1ankeel <r1ankeel@gmail.com>
2019-12-18 14:33:30 +0300

    initiate project

---
+++ script/script.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+
+  const customer = document.getElementById("customer"),
+      freelancer = document.getElementById("freelancer"),
+      blockCustomer = document.getElementById("block-customer"),
+      blockFreelancer = document.getElementById("block-freelancer"),
+      blockChoice = document.getElementById("block-choice"),
+      btnExit = document.getElementById("btn-exit"),
+      formCustomer = document.getElementById("form-customer"),
+      ordersTable = document.getElementById("orders");
+
+  const orders = [];
+
+  customer.addEventListener("click", () => {
+    blockCustomer.style.display = "block";
+    blockChoice.style.display = "none";
+    btnExit.style.display = "block";
+  });
+
+  freelancer.addEventListener("click", () => {
+    blockFreelancer.style.display = "block";
+    blockChoice.style.display = "none";
+    btnExit.style.display = "block";
+  });
+
+  btnExit.addEventListener("click", () => {
+    btnExit.style.display = "none";
+    blockFreelancer.style.display = "none";
+    blockCustomer.style.display = "none";
+    blockChoice.style.display = "block";
+  });
+
+  formCustomer.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const obj = {};
+
+    const elements = [...formCustomer.elements]
+    .filter(elem => (elem.tagName === "INPUT" && elem.type !== "radio") ||
+        (elem.type === "radio" && elem.checked) ||
+        elem.tagName === "TEXTAREA");
+   
+    elements.forEach(elem => {
+     
+        obj[elem.name] = elem.value;
+
+    });
+
+    formCustomer.reset();
+
+    orders.push(obj);
+    console.log(orders);
+  });
+
+
+});
